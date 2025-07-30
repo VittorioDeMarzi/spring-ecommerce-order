88ed8d8fb0a540057646570c281e9fc022e2e083
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-30 12:24:48 +0200

    refactor(Product): change type to data class

--- src/main/kotlin/ecommerce/model/Product.kt
+++ src/main/kotlin/ecommerce/model/Product.kt
@@ -1,36 +1,24 @@
 package ecommerce.model
 
 import ecommerce.dto.ProductResponse
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
 
-class Product(
-    val id: Long,
+@Entity
+data class Product(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+    @Column(nullable = false, unique = true)
     val name: String,
+    @Column(nullable = false)
     val price: Double,
     val imageUrl: String,
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Product
-
-        if (id != other.id) return false
-        if (price != other.price) return false
-        if (name != other.name) return false
-        if (imageUrl != other.imageUrl) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = id.hashCode()
-        result = 31 * result + price.hashCode()
-        result = 31 * result + name.hashCode()
-        result = 31 * result + imageUrl.hashCode()
-        return result
-    }
-}
+)
 
 fun Product.toDto(): ProductResponse {
-    return ProductResponse(id, name, price, imageUrl)
+    return ProductResponse(id!!, name, price, imageUrl)
 }
