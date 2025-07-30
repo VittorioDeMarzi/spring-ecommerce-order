ff712bf69fc500c567163a62ddcceb47e115da1b
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-30 19:46:56 +0200

    feat(ProductJpaRepository): implement getByIdOrThrow function

--- src/main/kotlin/ecommerce/repository/ProductJpaRepository.kt
+++ src/main/kotlin/ecommerce/repository/ProductJpaRepository.kt
@@ -1,7 +1,13 @@
 package ecommerce.repository
 
+import ecommerce.exception.ProductNotFoundException
 import ecommerce.model.Product
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.findByIdOrNull
+
+fun ProductJpaRepository.getByIdOrThrow(id: Long): Product =
+    findByIdOrNull(id)
+        ?: throw ProductNotFoundException("Product not found")
 
 interface ProductJpaRepository : JpaRepository<Product, Long> {
     fun existsByName(name: String): Boolean
