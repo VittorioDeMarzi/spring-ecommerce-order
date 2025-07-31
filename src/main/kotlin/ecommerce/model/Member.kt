8ac8b35feeaa43057840b1c93c9950f820d8983d
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-31 17:40:49 +0200

    refactor: move id as last property

--- src/main/kotlin/ecommerce/model/Member.kt
+++ src/main/kotlin/ecommerce/model/Member.kt
@@ -8,13 +8,13 @@ import jakarta.persistence.Id
 
 @Entity
 class Member(
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long = 0L,
     val name: String,
     val email: String,
     val password: String,
-    val role: String,
+    val role: String = "USER",
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0L,
 ) {
     fun toDto(): MemberDto {
         return MemberDto(id, email, role)
