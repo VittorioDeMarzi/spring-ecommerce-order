54362ef6746f3f3a12fdf81973047707417dbdf8
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-08-03 16:17:03 +0200

    feat: implement CartItemMapper.kt class and import it in CartService.kt

---
+++ src/main/kotlin/ecommerce/mapper/CartItemMapper.kt
@@ -0,0 +1,15 @@
+package ecommerce.mapper
+
+import ecommerce.dto.CartItemResponse
+import ecommerce.model.CartItem
+
+class CartItemMapper
+
+fun CartItem.toDto(): CartItemResponse =
+    CartItemResponse(
+        product.id,
+        productName = product.name,
+        quantity = quantity,
+        productPrice = product.price,
+        productImageUrl = product.imageUrl,
+    )
