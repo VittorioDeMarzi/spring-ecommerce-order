1a362858179ee5e58d4cffb72af7f4e1e74f167d
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-08-03 18:39:57 +0200

    refactor: add validation for column nullable false

--- src/main/kotlin/ecommerce/model/Member.kt
+++ src/main/kotlin/ecommerce/model/Member.kt
@@ -1,5 +1,6 @@
 package ecommerce.model
 
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
@@ -7,8 +8,11 @@ import jakarta.persistence.Id
 
 @Entity
 class Member(
+    @Column(nullable = false)
     val name: String,
+    @Column(nullable = false, unique = true)
     val email: String,
+    @Column(nullable = false)
     val password: String,
     val role: String = "USER",
     @Id
--- src/main/kotlin/ecommerce/model/Option.kt
+++ src/main/kotlin/ecommerce/model/Option.kt
@@ -1,6 +1,7 @@
 package ecommerce.model
 
 import ecommerce.exception.InsufficientQuantityException
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
@@ -10,7 +11,9 @@ import jakarta.persistence.ManyToOne
 
 @Entity
 class Option(
+    @Column(nullable = false)
     val name: String,
+    @Column(nullable = false)
     var quantity: Int,
     @ManyToOne
     @JoinColumn(name = "product_id")
@@ -19,6 +22,10 @@ class Option(
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0L,
 ) {
+    init {
+        require(quantity >= 0) { "Quantity cannot be negative" }
+    }
+
     fun reduceOptionQuantity(value: Int) {
         if (quantity < value) {
             throw InsufficientQuantityException("Insufficient quantity: $quantity")
