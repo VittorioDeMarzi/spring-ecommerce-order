caad71e1f36673a77678d828a8e074c4abc69e36
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-08-04 15:09:45 +0200

    fix: remove relation CartItem - CartHistory.kt. Now cart history has a Member and the Product

--- src/main/kotlin/ecommerce/model/CartHistory.kt
+++ src/main/kotlin/ecommerce/model/CartHistory.kt
@@ -4,22 +4,26 @@ import ecommerce.dto.enum.CartHistoryStatus
 import jakarta.persistence.Entity
 import jakarta.persistence.EnumType
 import jakarta.persistence.Enumerated
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
-import jakarta.persistence.OneToOne
+import jakarta.persistence.ManyToOne
 import org.hibernate.annotations.CreationTimestamp
 import java.time.LocalDateTime
 
 @Entity
 data class CartHistory(
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long,
-    @OneToOne
-    val cartProduct: CartItem,
+    @ManyToOne(fetch = FetchType.LAZY)
+    var member: Member,
+    @ManyToOne(fetch = FetchType.LAZY)
+    var product: Product,
+    var quantity: Int,
     @Enumerated(EnumType.STRING)
-    val status: CartHistoryStatus,
+    val status: CartHistoryStatus = CartHistoryStatus.ADDED,
     @CreationTimestamp
     var createdAt: LocalDateTime = LocalDateTime.now(),
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0L,
 )
