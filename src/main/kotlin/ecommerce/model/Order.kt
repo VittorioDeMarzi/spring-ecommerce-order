8ccee39dfc3a532c45038dd63ab2d627358b1e73
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-08-07 16:40:32 +0200

    feat(Order): remove relation with member. Use just memberId

--- src/main/kotlin/ecommerce/model/Order.kt
+++ src/main/kotlin/ecommerce/model/Order.kt
@@ -2,11 +2,9 @@ package ecommerce.model
 
 import jakarta.persistence.CascadeType
 import jakarta.persistence.Entity
-import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
-import jakarta.persistence.ManyToOne
 import jakarta.persistence.OneToMany
 import jakarta.persistence.Table
 import org.hibernate.annotations.CreationTimestamp
@@ -16,8 +14,7 @@ import java.time.LocalDateTime
 @Entity
 @Table(name = "customer_orders")
 class Order(
-    @ManyToOne(fetch = FetchType.LAZY)
-    val member: Member,
+    val memberId: Long,
     @OneToMany(cascade = [CascadeType.ALL])
     val orderItems: List<OrderItem>,
     @CreationTimestamp
