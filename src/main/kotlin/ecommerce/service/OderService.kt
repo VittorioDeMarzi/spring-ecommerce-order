5fe4b5e75cf1bec121b7bac344bfa86c04531ddc
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-08-10 12:09:17 +0200

    refactor(Order): Clear cart after order is succeeded

--- src/main/kotlin/ecommerce/service/OderService.kt
+++ src/main/kotlin/ecommerce/service/OderService.kt
@@ -13,20 +13,23 @@ import ecommerce.repository.OrderJpaRepository
 import ecommerce.repository.getByMemberId
 import org.hibernate.service.spi.ServiceException
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import java.math.BigDecimal
 
+@Transactional
 @Service
 class OderService(
     private val cartJpaRepository: CartJpaRepository,
     private val stripeClient: StripeClient,
     private val orderJpaRepository: OrderJpaRepository,
+    private val cartService: CartService,
 ) {
     fun processOrder(
         memberId: Long,
         request: OrderRequest,
     ): OrderResponse {
         val cart = cartJpaRepository.getByMemberId(memberId)
-        val amount = cart.cartProducts.sumOf { it.option.product!!.price }
+        val amount = cart.totalAmount
         val paymentIntent =
             stripeClient.createCheckoutSession(request, amount.multiply(BigDecimal.valueOf(100)).toInt())
                 ?: throw ServiceException("Payment Failed")
@@ -34,6 +37,7 @@ class OderService(
         val newOrder = placeOrder(cart, memberId)
         if (paymentIntent.status == "succeeded") {
             newOrder.status == OrderStatus.PAID
+            cart.cleanCart()
         }
         return newOrder.toOrderResponse()
     }
