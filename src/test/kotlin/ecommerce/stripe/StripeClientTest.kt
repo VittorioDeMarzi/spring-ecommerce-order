c374e0facb4386f41ad05272d012615a3c0feb40
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-08-10 20:20:56 +0200

    test: disable DB initialization for StripeClientTest

--- src/test/kotlin/ecommerce/stripe/StripeClientTest.kt
+++ src/test/kotlin/ecommerce/stripe/StripeClientTest.kt
@@ -10,7 +10,12 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+@SpringBootTest(
+    properties = [
+        "spring.sql.init.mode=never",
+        "spring.jpa.hibernate.ddl-auto=none",
+    ],
+)
 class StripeClientTest {
     @Autowired
     private lateinit var stripeClient: StripeClient
